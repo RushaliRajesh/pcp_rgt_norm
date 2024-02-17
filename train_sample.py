d493fde34b7110240a00d3abcb14580f8a83a486
RushaliRajesh <rushalirajesh2000@gmail.com>
2024-02-17 15:29:01 +0530

    first commit

---
+++ train_sample.py
@@ -0,0 +1,194 @@
+from __future__ import print_function
+
+import argparse
+import os
+import sys
+import random
+import math
+import shutil
+import torch
+import torch.nn.parallel
+import torch.optim as optim
+import torch.optim.lr_scheduler as lr_scheduler
+import torch.utils.data
+import pdb
+# from model import CNN
+import numpy as np
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch.nn.functional as F
+
+# from tensorboardX import SummaryWriter # https://github.com/lanpa/tensorboard-pytorch
+# from dataset import PointcloudPatchDataset, RandomPointcloudPatchSampler, SequentialShapeRandomPointcloudPatchSampler
+# from dataset_impro import PointcloudPatchDataset, RandomPointcloudPatchSampler, SequentialShapeRandomPointcloudPatchSampler
+from dataset_posenc import PointcloudPatchDataset, RandomPointcloudPatchSampler, SequentialShapeRandomPointcloudPatchSampler
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+
+    # naming / file handling
+    parser.add_argument('--name', type=str, default='my_single_scale_normal', help='training run name')
+    parser.add_argument('--desc', type=str, default='My training run for single-scale normal estimation.', help='description')
+    parser.add_argument('--indir', type=str, default='./pclouds', help='input folder (point clouds)')
+    parser.add_argument('--outdir', type=str, default='./models', help='output folder (trained models)')
+    parser.add_argument('--logdir', type=str, default='./logs', help='training log folder')
+    parser.add_argument('--trainset', type=str, default='trainingset_whitenoise.txt', help='training set file name')
+    parser.add_argument('--testset', type=str, default='validationset_whitenoise.txt', help='test set file name')
+    parser.add_argument('--saveinterval', type=int, default='10', help='save model each n epochs')
+    parser.add_argument('--refine', type=str, default='', help='refine model at this path')
+    parser.add_argument('--gpu_idx', type=int, default=0, help='set < 0 to use CPU')
+
+    # training parameters
+    parser.add_argument('--nepoch', type=int, default=2000, help='number of epochs to train for')
+    parser.add_argument('--batchSize', type=int, default=32, help='input batch size')
+    parser.add_argument('--patch_radius', type=float, default=[0.05], nargs='+', help='patch radius in multiples of the shape\'s bounding box diagonal, multiple values for multi-scale.')
+    parser.add_argument('--patch_center', type=str, default='point', help='center patch at...\n'
+                        'point: center point\n'
+                        'mean: patch mean')
+    parser.add_argument('--patch_point_count_std', type=float, default=0, help='standard deviation of the number of points in a patch')
+    parser.add_argument('--patches_per_shape', type=int, default=1000, help='number of patches sampled from each shape in an epoch')
+    parser.add_argument('--workers', type=int, default=1, help='number of data loading workers - 0 means same thread as main execution')
+    parser.add_argument('--cache_capacity', type=int, default=100, help='Max. number of dataset elements (usually shapes) to hold in the cache at the same time.')
+    parser.add_argument('--seed', type=int, default=3627473, help='manual seed')
+    parser.add_argument('--training_order', type=str, default='random', help='order in which the training patches are presented:\n'
+                        'random: fully random over the entire dataset (the set of all patches is permuted)\n'
+                        'random_shape_consecutive: random over the entire dataset, but patches of a shape remain consecutive (shapes and patches inside a shape are permuted)')
+    parser.add_argument('--identical_epochs', type=int, default=False, help='use same patches in each epoch, mainly for debugging')
+    parser.add_argument('--lr', type=float, default=0.0001, help='learning rate')
+    parser.add_argument('--momentum', type=float, default=0.9, help='gradient descent momentum')
+    parser.add_argument('--use_pca', type=int, default=False, help='Give both inputs and ground truth in local PCA coordinate frame')
+    parser.add_argument('--normal_loss', type=str, default='ms_euclidean', help='Normal loss type:\n'
+                        'ms_euclidean: mean square euclidean distance\n'
+                        'ms_oneminuscos: mean square 1-cos(angle error)')
+
+    # model hyperparameters
+    parser.add_argument('--outputs', type=str, nargs='+', default=['unoriented_normals'], help='outputs of the network, a list with elements of:\n'
+                        'unoriented_normals: unoriented (flip-invariant) point normals\n'
+                        'oriented_normals: oriented point normals\n'
+                        'max_curvature: maximum curvature\n'
+                        'min_curvature: mininum curvature')
+    parser.add_argument('--use_point_stn', type=int, default=True, help='use point spatial transformer')
+    parser.add_argument('--use_feat_stn', type=int, default=True, help='use feature spatial transformer')
+    parser.add_argument('--sym_op', type=str, default='max', help='symmetry operation')
+    parser.add_argument('--point_tuple', type=int, default=1, help='use n-tuples of points as input instead of single points')
+    parser.add_argument('--points_per_patch', type=int, default=500, help='max. number of points per patch')
+
+    return parser.parse_args()
+
+def cos_angle(v1, v2):
+    v1 = nn.functional.normalize(v1, dim=1)
+    v2 = nn.functional.normalize(v2, dim=1)
+    # pdb.set_trace()
+    return torch.bmm(v1.unsqueeze(1), v2.unsqueeze(2)).view(-1) / torch.clamp(v1.norm(2, 1) * v2.norm(2, 1), min=0.000001)
+    
+def loss_func(pred, target):
+    loss = (1-cos_angle(pred, target)).pow(2).mean() 
+    # pdb.set_trace()
+    return loss
+
+def rms_angular_error(estimated_normals, ground_truth_normals):
+    estimated_normals = F.normalize(estimated_normals, dim=1)
+    ground_truth_normals = F.normalize(ground_truth_normals, dim=1)
+
+    dot_product = torch.sum(estimated_normals * ground_truth_normals, dim=1)
+    dot_product = torch.clamp(dot_product, -1.0, 1.0)
+    angular_diff = torch.acos(dot_product) * torch.div(180.0, torch.pi)
+    squared_diff = angular_diff.pow(2)
+    mean_squared_diff = torch.mean(squared_diff)
+    rms_angular_error = torch.sqrt(mean_squared_diff)
+
+    return rms_angular_error.item()  
+
+def train_pcpnet(opt):
+
+    device = torch.device("cpu" if opt.gpu_idx < 0 else "cuda:%d" % opt.gpu_idx)
+
+    # colored console output
+    green = lambda x: '\033[92m' + x + '\033[0m'
+    blue = lambda x: '\033[94m' + x + '\033[0m'
+
+    log_dirname = os.path.join(opt.logdir, opt.name)
+    params_filename = os.path.join(opt.outdir, '%s_params.pth' % (opt.name))
+    model_filename = os.path.join(opt.outdir, '%s_model.pth' % (opt.name))
+    desc_filename = os.path.join(opt.outdir, '%s_description.txt' % (opt.name))
+
+    if os.path.exists(log_dirname) or os.path.exists(model_filename):
+        response = input('A training run named "%s" already exists, overwrite? (y/n) ' % (opt.name))
+        if response == 'y':
+            if os.path.exists(log_dirname):
+                shutil.rmtree(os.path.join(opt.logdir, opt.name))
+        else:
+            sys.exit()
+
+    # get indices in targets and predictions corresponding to each output
+    target_features = []
+    output_target_ind = []
+    output_pred_ind = []
+    output_loss_weight = []
+    pred_dim = 0
+    for o in opt.outputs:
+        if o == 'unoriented_normals' or o == 'oriented_normals':
+            if 'normal' not in target_features:
+                target_features.append('normal')
+
+            output_target_ind.append(target_features.index('normal'))
+            output_pred_ind.append(pred_dim)
+            output_loss_weight.append(1.0)
+            pred_dim += 3
+        elif o == 'max_curvature' or o == 'min_curvature':
+            if o not in target_features:
+                target_features.append(o)
+
+            output_target_ind.append(target_features.index(o))
+            output_pred_ind.append(pred_dim)
+            if o == 'max_curvature':
+                output_loss_weight.append(0.7)
+            else:
+                output_loss_weight.append(0.3)
+            pred_dim += 1
+        else:
+            raise ValueError('Unknown output: %s' % (o))
+
+    if pred_dim <= 0:
+        raise ValueError('Prediction is empty for the given outputs.')
+
+
+    if opt.seed < 0:
+        opt.seed = random.randint(1, 10000)
+
+    print("Random Seed: %d" % (opt.seed))
+    random.seed(opt.seed)
+    torch.manual_seed(opt.seed)
+
+    import time
+    
+    # create train and test dataset loaders
+    # pdb.set_trace()
+    train_dataset = PointcloudPatchDataset(
+        task = 'mse_6_4params_no_pos',
+        root=opt.indir,
+        shape_list_filename=opt.trainset,
+        patch_radius=opt.patch_radius,
+        points_per_patch=opt.points_per_patch,
+        patch_features=target_features,
+        point_count_std=opt.patch_point_count_std,
+        seed=opt.seed,
+        identical_epochs=opt.identical_epochs,
+        use_pca=opt.use_pca,
+        center=opt.patch_center,
+        point_tuple=opt.point_tuple,
+        cache_capacity=opt.cache_capacity)
+    
+    train_datasampler = RandomPointcloudPatchSampler(
+        train_dataset,
+        patches_per_shape=opt.patches_per_shape,
+        seed=opt.seed,
+        identical_epochs=opt.identical_epochs)
+    
+    train_dataset[3745]
+    
+    pdb.set_trace()
+
+if __name__ == '__main__':
+    train_opt = parse_arguments()
+    train_pcpnet(train_opt)
